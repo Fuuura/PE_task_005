2891edd9d258682271a361d791711e754d85f1e9
Aleksey Furaev <Fuuura@mail.ru>
2024-03-12 18:16:41 +0300

    house

---
+++ src/house.c
@@ -0,0 +1,27 @@
+#include "house.h"
+
+int main() {
+    int width = 0, length = 0, height = 0, money = 0;
+    input_data(&width, &length, &height, &money);
+    calculating(width, length, height, money);
+    exit(EXIT_SUCCESS);
+}
+
+void input_data(int *width, int *length, int *height, int *money) {
+    if ((scanf("%d %d %d %d", width, length, height, money) != 4) || *width < 0 || *length < 0 ||
+        *height < 0 || *money < 0) {
+        fck();
+        exit(EXIT_FAILURE);
+    }
+}
+
+void calculating(int width, int length, int height, int money) {
+    int price = (width * length * 1000) + 2 * (width * height * 500) + 2 * (length * height * 500) +
+                ((width * length * 700) + height * 100);
+    if (money >= price)
+        printf("YES");
+    else
+        printf("NO %d", price - money);
+}
+
+void fck() { fprintf(stderr, "Puck you, Verter!"); }
---
+++ src/house.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void input_data(int *width, int *length, int *height, int *money);
+void calculating(int width, int length, int height, int money);
+void fck();
